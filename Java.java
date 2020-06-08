481acfe1de86659742c62c31c43dfeb1f611fd86
darkhork <49198899+darkhork@users.noreply.github.com>
2020-06-08 09:17:44 +0900

    Add files via upload

---
+++ Java.java
@@ -0,0 +1,18 @@
+
+public class Java {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=0;i<11;i++)
+		{
+			sum+=i;
+		}
+		System.out.print(sum);
+		
+
+	}
+
+}
